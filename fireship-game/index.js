e87bf8c20030b086cebc3c01b55833277f8cfa55
stark5200 <a.taha5200@gmail.com>
2024-07-26 01:31:40 +0300

    fireship game setup + chalk green crazy frog test

---
+++ fireship-game/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+// above command tells th OS to execute the code with node.js version the user has installed
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+console.log(chalk.bgGreen("Crazy frog"));
