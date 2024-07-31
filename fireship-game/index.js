36bae65be95565db68a6e4f8a8319579ab69b3de
stark5200 <a.taha5200@gmail.com>
2024-07-31 23:03:13 +0200

    wrong commit

--- fireship-game/index.js
+++ fireship-game/index.js
@@ -45,17 +45,4 @@ async function welcome() {
 //await welcome()
 //await askName()
 
-/*
-As you know, git log shows you the history of commits in your repo. There are a few flags I like to use from time to time to make the output easier to read.
 
-The first is --decorate. It can be one of:
-
-short (the default)
-full (shows the full ref name)
-no (no decoration)
-Run git log --decorate=full. You should see that instead of just using your branch name, it will show the full ref name. A ref is just a pointer to a commit. All branches are refs, but not all refs are branches.
-
-Run git log --decorate=no. You should see that the branch names are no longer shown at all.
-
-The second is --oneline. This flag will show you a more compact view of the log. I use this one all the time, it just makes it so much easier to see what's going on.
-*/
