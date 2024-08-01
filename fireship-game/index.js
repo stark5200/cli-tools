22bdc55f26d66fda8f652563ada53046af0f0f8c
stark5200 <a.taha5200@gmail.com>
2024-08-01 02:20:24 +0200

    question 1 and handle answer

--- fireship-game/index.js
+++ fireship-game/index.js
@@ -10,7 +10,7 @@ import { createSpinner } from "nanospinner";
 
 console.log(chalk.bgGreen("Crazy frog"));
 
-let playerName;
+let playerName = "Player 1";
 
 const sleep  = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 async function welcome() {
@@ -25,24 +25,51 @@ async function welcome() {
     if you get any question wrong I will be ${chalk.bgRed('killed')}
     so get all the questions right...
     `)
-
-    async function askName() {
-      const answers = await inquirer.prompt({
-        name: 'stark', 
-        type: 'input', 
-        message: 'What is your role?', 
-
-        default() {
-          return 'Player';
-        },
-      });
-      
-      playerName = answers.player_name
-    }
-
 }
 
-//await welcome()
-//await askName()
+async function askName() {
+  const answers = await inquirer.prompt({
+    name: 'player_name', 
+    type: 'input', 
+    message: 'What is your name?', 
+    default() {
+      return 'Player 1';
+    },
+  });
+  
+  playerName = answers.player_name
+}
+
+async function question1() {
+  const answers = await inquirer.prompt({
+    name: 'question_1', 
+    type: 'list', 
+    message: 'Javascript was created in 10 days on\n',
+    choices: [
+      'May 23 , 1995',
+      'Nov 24, 1995',
+      'Dec 4, 1995',
+      'Dec 17, 1996',
+    ],
+  });
+
+  return handleAnswer(answers.question_1 == 'Dec 4, 1995');
+}
+
+async function handleAnswer(isCorrect) {
+  const spinner = createSpinner('Checking answer...').start();
+  await sleep();
+
+  if(isCorrect) {
+    spinner.success({text: `Nice work ${playerName}. That's a legit answer.`});
+  } else {
+    spinner.error({text: `💀💀💀 Game Over, you lose ${playerName}! `});
+    process.exit(1);
+  }
+}
+
+await welcome()
+await askName()
+await question1()
 
 
